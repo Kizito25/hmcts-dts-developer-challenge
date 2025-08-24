f5dbb2f3900d6b54132b95f4350421cdaca8d07b
Kizito25 <kizito25@gmail.com>
2025-08-24 14:54:54 +0100

    Add: Can_Create_a_Task Test

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Enums\TaskStatus;
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+
 
 class TaskTest extends TestCase
 {
@@ -18,5 +21,64 @@ class TaskTest extends TestCase
 
         $this->withoutExceptionHandling();
         $response->assertStatus(200);
+    }
+    /**
+     * Task Tests
+     * */
+
+    // Helper for JSON structure used by TaskResource
+    function taskJsonStructure(): array
+    {
+        return [
+            'id',
+            'title',
+            'description',
+            'status',
+            'due_date',
+            'is_overdue',
+            'created_at',
+            'updated_at',
+        ];
+    }
+    public function test_can_create_a_task(): void
+    {
+        // $res = $this->postJson('/api/tasks', $payload)
+        //     ->assertCreated()
+        //     ->assertJsonStructure([
+        //         'data' => [
+        //             'id',
+        //             'title',
+        //             'description',
+        //             'status',
+        //             'due_at',
+        //             'is_overdue',
+        //             'created_at',
+        //             'updated_at'
+        //         ]
+        //     ])
+        //     ->assertJsonPath('data.title', $payload['title']);
+
+        $data = [
+            'title' => 'Test Task',
+            'description' => 'This is a test task',
+            'status' => TaskStatus::TODO->value,
+            'due_date' => now()->addDay()->toDateTimeString(),
+        ];
+
+        $response = $this->postJson('api/tasks', $data);
+
+        $response->assertStatus(201)
+            ->assertJsonStructure($this->taskJsonStructure())
+            ->assertJsonFragment([
+                'title' => 'Test Task',
+                'description' => 'This is a test task',
+                'status' => TaskStatus::TODO->value,
+            ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'title' => 'Test Task',
+            'description' => 'This is a test task',
+            'status' => TaskStatus::TODO->value,
+        ]);
     }
 }
